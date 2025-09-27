f217d38f42fed42b633d8fefca3cfeb0e36e3aef
Edson Costa <edsonrcoosta@gmail.com>
2025-09-27 11:41:55 -0300

    feat: versão 2.0.0 - Grandes melhorias e novas funcionalidades
    - Atualização para versão 2.0.0
    - Sincronização da versão na interface
    - Preparação para novas funcionalidades da versão 2.0

--- config-ui/src/config.js
+++ config-ui/src/config.js
@@ -1,6 +1,6 @@
 // Arquivo de configuração da aplicação
 export const APP_CONFIG = {
-    version: '1.0.9',
+    version: '2.0.0',
     name: 'PDF Commander',
     description: 'Processador completo de PDFs com interface nativa integrada'
 };
